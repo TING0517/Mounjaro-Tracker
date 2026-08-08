c3a9cef298d72dc9b7dd6ba260660ea75e41bcf3
TING0517 <a2810030@gmail.com>
2026-08-08 15:07:28 +0800

    打包網頁版

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // For GitHub Pages deployment
+  base: './Mounjaro-Tracker/', // For GitHub Pages deployment
   plugins: [
     react(),
     tailwindcss(),
